0b17a5be0e5c1ff5d12e2f538f7262fcdf7d4180
hipak <hpk393@gmail.com>
2019-12-17 11:57:43 +0545

    ViewPagerCompleted!

--- app/src/main/java/com/softwarica/androidsecondassignment/hipak/ViewPagerActivity.java
+++ app/src/main/java/com/softwarica/androidsecondassignment/hipak/ViewPagerActivity.java
@@ -1,18 +1,43 @@
 package com.softwarica.androidsecondassignment.hipak;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
 import com.softwarica.androidsecondassignment.R;
+import com.softwarica.androidsecondassignment.hipak.adapter.ViewPagerAdapter;
+import com.softwarica.androidsecondassignment.hipak.fragments.area;
+import com.softwarica.androidsecondassignment.hipak.fragments.armstrong;
+import com.softwarica.androidsecondassignment.hipak.fragments.automorphic;
+import com.softwarica.androidsecondassignment.hipak.fragments.palindrome;
+import com.softwarica.androidsecondassignment.hipak.fragments.simpleinterest;
+import com.softwarica.androidsecondassignment.hipak.fragments.swapnumbers;
 
 public class ViewPagerActivity extends AppCompatActivity {
-    private ViewPager viewPager;
-    private TabLayout tabLayout;
+   private ViewPager viewPager;
+   private TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_page);
+
+        viewPager=findViewById(R.id.viewPager);
+        tabLayout=findViewById(R.id.tabid);
+
+        tabLayout.setSelectedTabIndicatorColor(Color.parseColor("#FFFFFF"));
+        ViewPagerAdapter viewPagerAdapter=new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new area(),"Area of Circle");
+        viewPagerAdapter.addFragment(new armstrong(),"ArmStrong");
+        viewPagerAdapter.addFragment(new automorphic(),"Automorphic");
+        viewPagerAdapter.addFragment(new swapnumbers()," Swap Numbers");
+        viewPagerAdapter.addFragment(new palindrome(),"Palindrome");
+        viewPagerAdapter.addFragment(new simpleinterest(),"Simple Interest");
+
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
 
 
     }
--- app/src/main/java/com/softwarica/androidsecondassignment/hipak/adapter/ViewPagerAdapter.java
+++ app/src/main/java/com/softwarica/androidsecondassignment/hipak/adapter/ViewPagerAdapter.java
@@ -5,7 +5,14 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ViewPagerAdapter extends FragmentPagerAdapter {
+   private List<Fragment> fragmentList=new ArrayList<>();
+   private List<String> fragmentTitle=new ArrayList<>();
+
+
     public ViewPagerAdapter(@NonNull FragmentManager fm) {
         super(fm);
     }
@@ -13,11 +20,18 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
     @NonNull
     @Override
     public Fragment getItem(int position) {
-        return null;
+        return fragmentList.get(position);
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return fragmentList.size();
+    }
+    public CharSequence getPageTitle(int position){
+        return fragmentTitle.get(position);
+    }
+    public void addFragment(Fragment fragment,String title){
+        fragmentList.add(fragment);
+        fragmentTitle.add(title);
     }
 }
